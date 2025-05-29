a61ccdb20c8e8613cb154fe677ff53c14f115529
zzz3375 <13694863375@163.com>
2025-05-29 18:13:07 +0800

    1

--- environment.sh
+++ environment.sh
@@ -4,6 +4,6 @@ conda activate DynamicNeRF
 pip3 install torch==2.5.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip3 install matplotlib tensorboard scipy opencv-python
 pip install imageio scikit-image configargparse timm lpips
- pip install ipdb
+pip install ipdb
 sudo apt install colmap
 sudo apt install graphicsmagick-imagemagick-compat
