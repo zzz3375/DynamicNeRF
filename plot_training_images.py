59254383c137a69aedbbc6ee216aa231e80e0017
zzz3375 <13694863375@163.com>
2025-07-11 23:09:22 +0800

    1

--- plot_training_images.py
+++ plot_training_images.py
@@ -112,32 +112,146 @@ def visualize_image_grid(log_path, tags_to_visualize, steps_to_visualize, save_p
     
     # plt.show()
 
+def compare_image_grids(
+    log_path1, steps1,
+    log_path2, steps2,
+    tags_to_visualize,
+    tags_to_display=None,
+    save_path='compare_image_grid'
+):
+    """
+    在同一网格中对比两个 TensorBoard 日志的图像
+    第一组在前 n 行，第二组在后 n 行
+    """
+    from tensorboard.backend.event_processing import event_accumulator
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from PIL import Image
+    import io
+    from pathlib import Path
+
+    n = len(tags_to_visualize)
+    cols = len(steps1)
+    assert len(steps1) == len(steps2), "两个步数列表长度必须一致"
+    
+    # 加载日志
+    ea1 = event_accumulator.EventAccumulator(log_path1, size_guidance={event_accumulator.IMAGES: 0})
+    ea1.Reload()
+    ea2 = event_accumulator.EventAccumulator(log_path2, size_guidance={event_accumulator.IMAGES: 0})
+    ea2.Reload()
+
+    fig, axes = plt.subplots( n, cols, figsize=(cols * 3,  n * 1.7))
+    if 2 * n == 1 and cols == 1:
+        axes = np.array([[axes]])
+    elif 2 * n == 1:
+        axes = axes.reshape(1, -1)
+    elif cols == 1:
+        axes = axes.reshape(-1, 1)
+
+    fontsize = 14
+    # LOG_PATH1
+    for i, tag in enumerate(tags_to_visualize):
+        tag_name = tags_to_display[i] if tags_to_display else tag
+        image_events = ea1.Images(tag)
+        for j, step in enumerate(steps1):
+            ax = axes[i, j]
+            image_event = next((e for e in image_events if e.step == step), None)
+            if image_event is not None:
+                img = Image.open(io.BytesIO(image_event.encoded_image_string))
+                ax.imshow(np.array(img), cmap='viridis' if "depth" in tag else None)
+                ax.set_xticks([])
+                ax.set_yticks([])
+                if j == 0:
+                    ax.set_ylabel(f"{tag_name}\n(mountain)", fontsize=fontsize)
+                if i == 0:
+                    ax.set_title(f"{str(step)[:-5]}00K", fontsize=fontsize)
+            else:
+                ax.text(0.5, 0.5, f"No Image\nTag: {tag}\nStep: {step}", ha='center', va='center', transform=ax.transAxes)
+                ax.set_facecolor('lightgray')
+    plt.tight_layout()
+    Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+    plt.savefig(save_path+"-mountain"+".png", dpi=300, bbox_inches='tight')
+    print(f"Compare grid saved to {save_path}")
+ 
+    # LOG_PATH2
+    fig, axes = plt.subplots( n, cols, figsize=(cols * 3,  n * 1.7))
+    if 2 * n == 1 and cols == 1:
+        axes = np.array([[axes]])
+    elif 2 * n == 1:
+        axes = axes.reshape(1, -1)
+    elif cols == 1:
+        axes = axes.reshape(-1, 1)
+
+    for i, tag in enumerate(tags_to_visualize):
+        tag_name = tags_to_display[i] if tags_to_display else tag
+        image_events = ea2.Images(tag)
+        for j, step in enumerate(steps2):
+            ax = axes[i, j]
+            image_event = next((e for e in image_events if e.step == step), None)
+            if image_event is not None:
+                img = Image.open(io.BytesIO(image_event.encoded_image_string))
+                ax.imshow(np.array(img), cmap='jet' if "depth" in tag else None)
+                ax.set_xticks([])
+                ax.set_yticks([])
+                if j == 0:
+                    ax.set_ylabel(f"{tag_name}\n(plain)", fontsize=fontsize)
+                if i == 0:
+                    ax.set_title(f"{str(step)[:-3]}K", fontsize=fontsize)
+            else:
+                ax.text(0.5, 0.5, f"No Image\nTag: {tag}\nStep: {step}", ha='center', va='center', transform=ax.transAxes)
+                ax.set_facecolor('lightgray')
+
+    plt.tight_layout()
+    Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+    plt.savefig(save_path+"-plain"+".png", dpi=300, bbox_inches='tight')
+    print(f"Compare grid saved to {save_path}")
+    del ea1, ea2
+
+# 用法示例
+# compare_image_grids(
+#     log_path1=LOG_PATH1,
+#     steps1=STEPS1,
+#     log_path2=LOG_PATH2,
+#     steps2=STEPS2,
+#     tags_to_visualize=TAGS_TO_VISUALIZE,
+#     tags_to_display=TAGS_TO_DISPLAY,
+#     save_path=
+
 if __name__ == "__main__":
     # 指定TensorBoard日志路径
-    LOG_PATH = 'logs/summaries/DJI_20250516151729_0005_V-100f-fps30_DyNeRF_pretrain_test/events.out.tfevents.1748093593.ZhizhangOFFICE.2938.0'
+    # LOG_PATH = 'logs/summaries/DJI_20250516151729_0005_V-100f-fps30_DyNeRF_pretrain_test/events.out.tfevents.1748093593.ZhizhangOFFICE.2938.0'
     
     # render
-    # TAGS_TO_VISUALIZE = ['rgb_holdout', 'rgb_s', 'rgb_d',]
-    # TAGS_TO_DISPLAY = ['Ground Truth', 'Render (NeRF)', 'Render (NeRF + D)']  # 标签显示名称
+    # TAGS_TO_VISUALIZE = [ 'rgb_s', 'rgb_d', 'rgb_holdout',]
+    # TAGS_TO_DISPLAY = [ 'NeRF', 'NeRF + D', 'Ground Truth',]  # 标签显示名称
 
     # depth
-    # TAGS_TO_VISUALIZE = ['rgb_holdout', 'depth_s', 'depth_d']
-    # TAGS_TO_DISPLAY = ['Ground Truth', 'Depth (NeRF)', 'Depth (NeRF + D)']  # 标签显示名称
+    TAGS_TO_VISUALIZE = [ 'rgb_holdout', 'depth_s', 'depth_d',]
+    TAGS_TO_DISPLAY = [ 'Ground Truth', 'NeRF', 'NeRF + D', ]  # 标签显示名称
 
     # flow
-    TAGS_TO_VISUALIZE = [ 'induced_flow_f', 'flow_f_gt', 'induced_flow_b', 'flow_b_gt', ]  # 需要可视化的图像标签
-    TAGS_TO_DISPLAY = ['Estimated displacements\n(forward)', 'RAFT dense-flow\n(forward)', 'Estimated displacements\n(backward)', 'RAFT dense-flow\n(backward)']  # 标签显示名称d
+    # TAGS_TO_VISUALIZE = [ 'induced_flow_f', 'flow_f_gt', 'induced_flow_b', 'flow_b_gt', ]  # 需要可视化的图像标签
+    # TAGS_TO_DISPLAY = ['Estimated displacements\n(forward)', 'RAFT dense-flow\n(forward)', 'Estimated displacements\n(backward)', 'RAFT dense-flow\n(backward)']  # 标签显示名称d
 
     # 指定要可视化的步数列表(作为列)
-    STEPS_TO_VISUALIZE = [173_000, 272_000, 394_500, 455_000, 500_000]  # 示例步数
+    # STEPS_TO_VISUALIZE = [173_000, 272_000, 394_500, 455_000, 500_000]  # 示例步数
     
     # 可视化并保存结果
-    visualize_image_grid(
-        log_path=LOG_PATH,
-        tags_to_visualize=TAGS_TO_VISUALIZE,
-        steps_to_visualize=STEPS_TO_VISUALIZE,
-        TAGS_TO_DISPLAY=TAGS_TO_DISPLAY,
-        save_path='tensorboard_image_grid.png' \
-        '',
+    # visualize_image_grid(
+    #     log_path=LOG_PATH,
+    #     tags_to_visualize=TAGS_TO_VISUALIZE,
+    #     steps_to_visualize=STEPS_TO_VISUALIZE,
+    #     TAGS_TO_DISPLAY=TAGS_TO_DISPLAY,
+    #     save_path='tensorboard_image_grid.png' \
+    #     '',
         
-    )
+    # )
+    compare_image_grids(
+    log_path1=r"logs/summaries/inservice-wind-turbine/events.out.tfevents.1752078591.ZhizhangOFFICE.123140.0",
+    steps1=[147_000, 233_500, 330_500, 470_000, 500_000],
+    log_path2=r"logs/summaries/plain-inservice/events.out.tfevents.1752147392.ZhizhangOFFICE.49301.0",
+    steps2=[103_000, 200_000, 339_500, 443_500,  500_000  ],
+    tags_to_visualize=TAGS_TO_VISUALIZE,
+    tags_to_display=TAGS_TO_DISPLAY,
+    save_path='compare_image_grid')
+
