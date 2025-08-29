96bb041dc05095c8484bf0c3a3ec543009bdae8e
zzz3375 <13694863375@163.com>
2025-08-29 22:02:18 +0800

    1

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -367,7 +367,7 @@ def get_rays(H, W, focal, c2w):
     # Rotate ray directions from camera frame to the world frame
     rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1) # dot product, equals to: [c2w.dot(dir) for dir in dirs]
     # Translate camera frame's origin to the world frame. It is the origin of all rays.
-    rays_o = c2w[:3, -1].expand(rays_d.shape)
+    rays_o = torch.Tensor(c2w[:3, -1]).expand(rays_d.shape)
     return rays_o, rays_d
 
 
