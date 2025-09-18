64ccfbe42f6a5c5c7902cf2aefbe423fe65be635
zzz3375 <13694863375@163.com>
2025-09-18 14:03:39 +0800

    1

--- environment.sh
+++ environment.sh
@@ -1,8 +1,8 @@
 conda create -n DynamicNeRF python=3.12 numpy=1.26.4 -y
 conda activate DynamicNeRF
 
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
-# pip3 install torch==2.5.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+# pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+pip3 install torch==2.5.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 # pip3 install torch==2.5.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip3 install matplotlib tensorboard scipy opencv-python pandas
 pip install imageio scikit-image configargparse timm lpips open3d
